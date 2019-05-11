857fc11427e4e4e5f20adc012e3323f39424112c
Daniil Soldatov <of.swg.csgo@gmail.com>
2019-05-11 15:04:16 +0700

    Add command-line arguments support

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
-int main() { return 0; }
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char **argv) {
+  if (argc == 1 || argc > 2) {
+    printf("Usage: /canalyze 'file.c'\n");
+    return 0;
+  }
+  const char *file = argv[1];
+  FILE *f = fopen(file, "r");
+  if (f == NULL) {
+    printf("Can't open file for read\n");
+    return -1;
+  }
+
+  return 0;
+}
