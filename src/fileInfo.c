ee616b68105fedc641cd871869418c6c3ba25b63
Daniil Soldatov <of.swg.csgo@gmail.com>
2019-05-13 15:59:04 +0700

    Adding functions to fill the variable "text" with a string from the file

--- src/fileInfo.c
+++ src/fileInfo.c
@@ -1,6 +1,25 @@
 #include "headers/fileInfo.h"
 #include <stdlib.h>
 
+size_t fileGetContents(FILE *f, sString *string) {
+  char *buffer = NULL;
+  size_t len;
+  size_t counter = 0;
+  while ((getline(&buffer, &len, f)) != -1) {
+    char *strbuf = malloc(len);
+    removeSpacesAtBegin(buffer);
+    char *pos;
+    if ((pos = strchr(buffer, '\n')) != 0) {
+      buffer[pos - buffer] = '\0';
+    }
+    strcpy(strbuf, buffer);
+    string[counter].text = strbuf;
+    string[counter].string_number = (unsigned int)counter;
+    counter++;
+  }
+  return counter;
+}
+
 size_t fileGetString(FILE *fp) {
   fpos_t fpos;
   fgetpos(fp, &fpos);
@@ -13,4 +32,21 @@ size_t fileGetString(FILE *fp) {
   free(buf);
   fsetpos(fp, &fpos);
   return size;
+}
+
+size_t removeSpacesAtBegin(char *string) {
+  int i = 0;
+  size_t ssize = (size_t)strlen(string);
+  while (string[i] == ' ') {
+    if (string[i] != ' ')
+      break;
+    i++;
+  }
+  ssize_t k = 0;
+  for (int j = i; j < ssize + 1; j++) {
+    string[k] = string[j];
+    k++;
+  }
+  ssize -= i;
+  return ssize;
 }
