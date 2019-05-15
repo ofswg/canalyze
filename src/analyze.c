9670bfd8db76213a31126c28f37ce95b3cc29bec
Daniil Soldatov <of.swg.csgo@gmail.com>
2019-05-15 16:16:55 +0700

    Attempting to deal with regular expressions

--- src/analyze.c
+++ src/analyze.c
@@ -6,7 +6,7 @@
 
 char *isFunction(stringArray strArray, int string_number) {
   char *nameF = malloc(sizeof(char));
-  char *regptr = "^([A-Za-z0-9_]+)";
+  char *regptr = "([a-zA-Z0-9_]+)";
 
   regex_t regex;
   int regi = regcomp(&regex, regptr, REG_EXTENDED);
@@ -17,9 +17,17 @@ char *isFunction(stringArray strArray, int string_number) {
   char *string_analyse = strArray.string[string_number].text;
   int status = regexec(&regex, string_analyse, 2, regmatch, 0);
   if (status) {
+    regfree(&regex);
     return NULL;
   }
-  printf("%d : %d \n", (int)regmatch[1].rm_eo, (int)regmatch[1].rm_so);
+  size_t j = 0;
+  for (size_t i = (int)regmatch[1].rm_so; i < (int)regmatch[1].rm_eo; i++) {
+    nameF[j] = strArray.string[string_number].text[i];
+    j++;
+  }
+  nameF[j] = '\0';
+  printf("%d : %d = %s\n", (int)regmatch[1].rm_eo, (int)regmatch[1].rm_so,
+         nameF);
 
   regfree(&regex);
   return nameF;
@@ -31,11 +39,11 @@ functionArray getFunctions(stringArray *strArray) {
   for (size_t i = 0; i < strArray->capacity; i++) {
     char *nameFunction;
     if ((nameFunction = isFunction(*strArray, (int)i)) != NULL) {
-      printf("%s\n", nameFunction);
+      // printf("%s\n", nameFunction);
       if (strchr(strArray->string[i].text, '{') != NULL) {
-        // printf("%s\n", strArray->string[i].text);
+        printf("%s\n", strArray->string[i].text);
       } else if (strchr(strArray->string[i + 1].text, '{') != NULL) {
-        // printf("%s\n", strArray->string[i + 1].text);
+        printf("%s\n", strArray->string[i + 1].text);
       } else {
         continue;
       }
