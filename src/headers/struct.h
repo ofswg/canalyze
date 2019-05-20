bc58304746448d0d8f87380c17eb6118b0ecb990
Daniil Soldatov <of.swg.csgo@gmail.com>
2019-05-20 17:58:57 +0700

    Removing extra structures

--- src/headers/struct.h
+++ src/headers/struct.h
@@ -1,23 +1,17 @@
 #ifndef STRUCT_H
 #define STRUCT_H
 
-typedef enum { NONE = 0, FUNCTION = 1, NON_FUNCTION = 2 } type;
-
 // Хранит тип строки
 
 typedef struct {
   unsigned int string_number; // Номер строки
   char *text;                 // Строка
-  type type[2];
 } sString;
 
 // Хранит информацию о функции
 
 typedef struct {
   char *function_name; // Имя функции
-  unsigned int str_num;
-  unsigned int open_bracket_string_number; // Номер строки открывающей скобки
-  unsigned int close_bracket_string_number; // Номер строки закрывающей скобки
 } sFunction;
 
 // Структуры для хранения массивов заданных типов
