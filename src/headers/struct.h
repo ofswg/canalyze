c7c5ad374d26b49ba6b771c7171160f544e9c209
Daniil Soldatov <of.swg.csgo@gmail.com>
2019-05-21 15:02:04 +0700

    Adding structures for task implementation with variables

--- src/headers/struct.h
+++ src/headers/struct.h
@@ -1,12 +1,32 @@
 #ifndef STRUCT_H
 #define STRUCT_H
 
+// Перечисление типов строк
+
+typedef enum {
+  NONE = 0,
+  FUNCTION = 1,
+  NON_FUNCTION = 2,
+  VAR_GLOBAL = 3,
+  VAR_NON_GLOBAL = 4
+} type;
+
 // Хранит тип строки
 
 typedef struct {
   unsigned int string_number; // Номер строки
   char *text;                 // Строка
+  type types[2];              // Тип строки
 } sString;
+
+// Хранит информацию о переменной
+
+typedef struct {
+  char *variable_name;        // Имя переменной
+  char *variable_value;       // Значение
+  unsigned int string_number; // Номер строки с переменной
+  type types;                 // Тип переменной
+} sVariable;
 
 // Хранит информацию о функции
 
@@ -25,6 +45,11 @@ typedef struct {
   sFunction *array;
   unsigned int capacity;
 } functionArray;
+
+typedef struct {
+  sVariable *array;
+  unsigned int capacity;
+} variableArray;
 
 functionArray getFunctions(stringArray *strArray);
 int getCallTimes(stringArray strArray, char *function_name);
